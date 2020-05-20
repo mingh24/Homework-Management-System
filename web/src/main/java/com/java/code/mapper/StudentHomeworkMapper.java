ab1da21ab04e5e54932220a0bd401087b15a4f78
yipple <1275432232@qq.com>
2020-05-21 01:18:35 +0800

    Mybatis

---
+++ web/src/main/java/com/java/code/mapper/StudentHomeworkMapper.java
@@ -0,0 +1,13 @@
+package com.java.code.mapper;
+
+/**
+ * Project Name: Homework-Management-System
+ * File Name: StudentHomeworkMapper
+ * Package Name: com.java.code.mapper
+ *
+ * @author yipple
+ * @date 2020/5/18
+ * @since 0.0.1
+ */
+public class StudentHomeworkMapper {
+}
---
+++ web/src/main/java/com/java/code/mapper/StudentMapper.java
@@ -0,0 +1,13 @@
+package com.java.code.mapper;
+
+/**
+ * Project Name: Homework-Management-System
+ * File Name: StudentMapper
+ * Package Name: com.java.code.mapper
+ *
+ * @author yipple
+ * @date 2020/5/18
+ * @since 0.0.1
+ */
+public class StudentMapper {
+}
